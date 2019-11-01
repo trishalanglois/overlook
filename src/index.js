95cf8b4c63dc1f0c3e37d456cb6892108f68b927
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-01 17:20:01 -0600

    Add input fields and toggle to main page

--- src/index.js
+++ src/index.js
@@ -9,4 +9,13 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
+// $('.nav-to-login').on('click', () => {
+//   window.location = './guest-page.html'
+// })
+
+$('.nav-to-login').click(function(){
+  $('#main-input').slideToggle();
+});
+
+
 console.log('This is the JavaScript entry file - your code begins here.');
