e7719aad3c4aaf65ab785d3da7d5a4a079e681c5
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-02 10:17:06 -0600

    Add fetch requests for bookings and rooms on initial page load

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import $ from 'jquery';
 import './css/base.scss';
 
-let roomsData = fetch() //complete fetch request
-let bookingsData = fetch() //complete fetch request
+let roomsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  .then(response => response.json())
+  .then(json => json.rooms)
+  .catch(error => console.log(error));
+let bookingsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  .then(response => response.json())
+  .then(json => json.bookings)
+  .catch(error => console.log(error));
 let tapechart;
 
 $('#login-button').on('click', () => {
   tapechart = new TapeChart(roomsData, bookingsData);
-  
+
   let username = $('.username').val();
   if (username === 'manager' && $('.password').val() === 'overlook2019') {
     window.location = './staff-page.html'
