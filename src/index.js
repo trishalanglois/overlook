332ae5cfbbaaed871f03b0b4b046186bbb9a8cd9
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-01 22:16:46 -0600

    Set up error message for login with styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import $ from 'jquery';
 import './css/base.scss';
 
+let roomsData = fetch() //complete fetch request
+let bookingsData = fetch() //complete fetch request
+let tapechart;
+
 $('#login-button').on('click', () => {
-  if ($('.username').val() === 'manager') {
+  tapechart = new TapeChart(roomsData, bookingsData);
+  
+  let username = $('.username').val();
+  if (username === 'manager' && $('.password').val() === 'overlook2019') {
     window.location = './staff-page.html'
-  } else {
+  } else if (username.includes('customer') && $('.password').val() === 'overlook2019') {
     window.location = './guest-page.html'
+  } else {
+    showLoginError();
   }
 })
 
 $('.nav-to-login').click(function(){
   $('#main-input').slideToggle();
 });
+
+function showLoginError() {
+  $('.error-message').show();
+}
