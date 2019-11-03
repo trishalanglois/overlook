1f48427e772d496242e3ad0c9832c1840d9ed2ed
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-03 09:50:27 -0700

    Set up stats for manager and display on DOM

--- src/index.js
+++ src/index.js
@@ -4,27 +4,25 @@ import TapeChart from './TapeChart';
 import Guest from './Guest';
 import Manager from './Manager';
 
+
 let tapechart;
 let today = new Date();
 let guestId;
-
-
+let allGuests;
 
 var date = JSON.stringify(today.getFullYear()+'/'+(today.getMonth()+1)+'/'+ JSON.stringify(today.getDate()).padStart(2, 0));
 
-// console.log(date);
+
+//FETCHES
 let roomsDataFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
 .then(response => response.json());
-
 let bookingsDataFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
 .then(response => response.json());
-
 let guestsDataFetch = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
 .then(response => response.json());
 
-let tapechartData = {roomsData: [], bookingsData: []};
-let allGuests;
 
+let tapechartData = {roomsData: [], bookingsData: []};
 Promise.all([roomsDataFetch, bookingsDataFetch, guestsDataFetch])
   .then(data => {
     tapechartData.roomsData = data[0].rooms;
@@ -35,15 +33,12 @@ Promise.all([roomsDataFetch, bookingsDataFetch, guestsDataFetch])
   .then(tapechartData => {
     console.log('this', tapechartData.roomsData, tapechartData.bookingsData);
     console.log('guests', allGuests);
-    return tapechart = new TapeChart(tapechartData.roomsData, tapechartData.bookingsData, date)
+    tapechart = new TapeChart(tapechartData.roomsData, tapechartData.bookingsData, date);
+    populateManagerPage(date);
   })
-  // .then(guestsData => {
-  //   console.log(guestsData);
-  //   return allGuests = guestsData;
 
-
+//LOGIN PAGE
 $('#login-button').on('click', () => {
-
   let username = $('.username').val();
   // console.log(guestId);
   if (username === 'manager' && $('.password').val() === 'overlook2019') {
@@ -59,12 +54,18 @@ $('#login-button').on('click', () => {
   }
 })
 
-
-
 $('.nav-to-login').click(function(){
   $('#main-input').slideToggle();
 });
 
 function showLoginError() {
   $('.error-message').show();
+}
+
+//MANAGER PAGE
+function populateManagerPage(date) {
+  $('#manager-available-rooms').text(tapechart.findAllAvailableRooms(date).length);
+  $('#manager-revenue').text('$' + tapechart.calculateDailyRevenue(date));
+  $('#manager-occupied-rooms').text(tapechart.findPercentAvailableRooms() + '%');
+
 }
